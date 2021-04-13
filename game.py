c0a30ba478f4a2d503b11b0dc965d2a2197a7542
Hamhams <hamhams86@gmail.com>
2021-04-13 15:23:53 -0700

    created first version of game_start function

--- game.py
+++ game.py
@@ -3,6 +3,13 @@ health = 3
 questions = [["what is 2 + 2", [4, [3, 5]]], ["What is 3 + 4", [7, [6, 8]]]]
 
 def game_start():
-    print("You have entered the dungeon. You must enter the door with the correct answer to each rooms questions in the hope of reaching the treasure before you reach your doom!")
+    print("You have entered the dungeon. Upon entering each room you must choose the door with the correct answer to each  questions in the hopes of reaching the treasure before you reach your doom!")
+    start = input("Are you prepared? (yes/no)").lower()
+    if start == "yes":
+        print("Then lettuce begin! (yeah, it's one of those kind of games...)")
+    else:
+        game_start()
+
+
 
 game_start()
