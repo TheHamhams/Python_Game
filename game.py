7801682f90569568c0f88fa3bd45e93dea54743e
Hamhams <hamhams86@gmail.com>
2021-04-13 15:13:57 -0700

    Actual first commit

--- game.py
+++ game.py
@@ -1,1 +1,8 @@
-print("Hello")
+active = True
+health = 3
+questions = [["what is 2 + 2", [4, [3, 5]]], ["What is 3 + 4", [7, [6, 8]]]]
+
+def game_start():
+    print("You have entered the dungeon. You must enter the door with the correct answer to each rooms questions in the hope of reaching the treasure before you reach your doom!")
+
+game_start()
